5a3dfc712c6049211752984cc0597e22776321df
Ridha Mezrigui <47607249+ridhamz@users.noreply.github.com>
2020-08-26 19:59:24 +0100

    Updates

---
+++ precache-manifest.0003479041bd2f189a07e9e75e62ad68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569e99c3a949ec9f6b772e013acaf4a1",
+    "url": "/react-burger-app/index.html"
+  },
+  {
+    "revision": "d3e33df40a389639ab3c",
+    "url": "/react-burger-app/static/css/main.60293342.chunk.css"
+  },
+  {
+    "revision": "8ffa3dcfe6cf09083075",
+    "url": "/react-burger-app/static/js/2.fecd6234.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-burger-app/static/js/2.fecd6234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e33df40a389639ab3c",
+    "url": "/react-burger-app/static/js/main.a778fccd.chunk.js"
+  },
+  {
+    "revision": "7c0fedbd37f06843105e",
+    "url": "/react-burger-app/static/js/runtime-main.8f04002e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
